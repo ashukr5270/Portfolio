a52259f299266d87f48bebded4d77ca4b63b7836
Ashish Kr <ashumgr0000@gmail.com>
2026-04-01 11:51:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const colors = ["#65ff4dff"];
 let colorIndex = 0;
 
 const typed = new Typed(".multiple-text", {
-  strings: ["Cloud Engineer", "Cloud Architect", "Cloud Consultant", "Cloud Administrator", "Cloud Support Engineer"]
+  strings: ["Cloud Engineer", "Cloud Architect", "Cloud Consultant", "Cloud Administrator"]
   typeSpeed: 80,
   backSpeed: 60,
   backDelay: 2000,
